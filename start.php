3811d359431522fb368650bac36e941e0002bb67
Cash Costello <cash.costello@gmail.com>
2011-12-12 19:16:56 -0500

    added consumer and server libs that can be loaded using Elgg's api

--- start.php
+++ start.php
@@ -1,6 +1,12 @@
 <?php
 /**
- *
+ * OpenID API Library
  */
 
+elgg_register_event_handler('init', 'system', 'openid_api_init');
 
+function openid_api_init() {
+	$dir = elgg_get_plugins_path() . 'openid_api/lib/';
+	elgg_register_library('openid_comsumer', "$dir/openid_consumer.php");
+	elgg_register_library('openid_server', "$dir/openid_server.php");
+}
